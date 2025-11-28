be63c51d2f794c9a62374290388c47a21865bc62
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2025-11-28 11:52:26 +0900

    #5 メインビジュアルスライダーのコンポーネント化

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,16 @@ import Info from '@/components/Info/Info'
 import ContentBlock from '@/components/ContentBlock/ContentBlock'
 import styles from './page.module.scss'
 
+const slides = [
+  { image: '/img/top/mv_2022SS.jpg', alt: '2022 Summer Season Collection', href: '#' },
+  { image: '/img/top/mv_dress.jpg', alt: 'Dress Collection', href: '#' },
+  { image: '/img/top/mv_Tshirt.jpg', alt: 'Tshirt Collection', href: '#' },
+]
+
 export default function Home() {
   return (
     <div className={styles.toppage}>
-      <MainVisual />
+      <MainVisual slides={slides} />
       <Info />
       <ContentBlock />
     </div>
--- components/MainVisual/MainVisual.tsx
+++ components/MainVisual/MainVisual.tsx
@@ -1,5 +1,74 @@
-import React from 'react'
+'use client'
 
-export default function MainVisual() {
-  return <div>MainVisual</div>
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Autoplay } from 'swiper/modules'
+import Link from 'next/link'
+import Image from 'next/image'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import styles from './MainVisual.module.scss'
+
+type Slide = {
+  image: string
+  alt: string
+  href?: string
+}
+
+type MainVisualProps = {
+  slides: Slide[]
+}
+
+export default function MainVisual({ slides }: MainVisualProps) {
+  return (
+    <div className={styles.main_visual}>
+      <div className={`${styles.main_visual_inner} content_width`}>
+        <div className={styles.main_visual_body}>
+          <p className={styles.main_visual_title}>APRICOT</p>
+          <p className={styles.main_visual_subtitle}>
+            Elegance
+            <br />&<br />
+            Mignonne
+          </p>
+        </div>
+        <Swiper
+          modules={[Pagination, Autoplay]}
+          pagination={{
+            clickable: true,
+            type: 'bullets',
+          }}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          speed={1300}
+          className={styles.slider01}
+        >
+          {slides.map((slide, index) => (
+            <SwiperSlide key={index}>
+              {slide.href ? (
+                <Link href={slide.href}>
+                  <Image
+                    src={slide.image}
+                    alt={slide.alt}
+                    width={800}
+                    height={600}
+                    priority={index === 0}
+                  />
+                </Link>
+              ) : (
+                <Image
+                  src={slide.image}
+                  alt={slide.alt}
+                  width={800}
+                  height={600}
+                  priority={index === 0}
+                />
+              )}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  )
 }
